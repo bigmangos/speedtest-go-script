93463fb281a375fa6c699eb5364ccb27ed7f0b8b
bigMango <ifenghuangxin@gmail.com>
2020-04-21 10:29:06 +0800

    Add setting port. fix #2

--- install.sh
+++ install.sh
@@ -9,108 +9,138 @@ export PATH
 dir="/usr/speedtest/"
 
 function setout(){
-	if [ -e "/usr/bin/yum" ]; then
-		yum -y install git wget
-	else
-		sudo apt-get update
-		sudo apt-get install -y wget git
-	fi
+    if [ -e "/usr/bin/yum" ]; then
+        yum -y install git wget
+    else
+        sudo apt-get update
+        sudo apt-get install -y wget git
+    fi
 }
 
 function chk_firewall(){
-	if [ -e "/etc/sysconfig/iptables" ]; then
-		iptables -I INPUT -p tcp --dport 8989 -j ACCEPT
-		service iptables save
-		service iptables restart
-	elif [ -e "/etc/firewalld/zones/public.xml" ]; then
-		firewall-cmd --zone=public --add-port=8989/tcp --permanent
-		firewall-cmd --reload
-	elif [ -e "/etc/ufw/before.rules" ]; then
-		sudo ufw allow 8989/tcp
-	fi
+    if [ -e "/etc/sysconfig/iptables" ]; then
+        iptables -I INPUT -p tcp --dport $port -j ACCEPT
+        service iptables save
+        service iptables restart
+    elif [ -e "/etc/firewalld/zones/public.xml" ]; then
+        firewall-cmd --zone=public --add-port=$port/tcp --permanent
+        firewall-cmd --reload
+    elif [ -e "/etc/ufw/before.rules" ]; then
+        sudo ufw allow $port/tcp
+    fi
 }
 
 function del_post() {
-	if [ -e "/etc/sysconfig/iptables" ]; then
-		sed -i '/^.*8989.*/'d /etc/sysconfig/iptables
-		service iptables save
-		service iptables restart
-	elif [ -e "/etc/firewalld/zones/public.xml" ]; then
-		firewall-cmd --zone=public --remove-port=8989/tcp --permanent
-		firewall-cmd --reload
-	elif [ -e "/etc/ufw/before.rules" ]; then
-		sudo ufw delete 8989/tcp
-	fi
+    if [ -e "/etc/sysconfig/iptables" ]; then
+        sed -i "/^.*$port.*/"d /etc/sysconfig/iptables
+        service iptables save
+        service iptables restart
+    elif [ -e "/etc/firewalld/zones/public.xml" ]; then
+        firewall-cmd --zone=public --remove-port=$port/tcp --permanent
+        firewall-cmd --reload
+    elif [ -e "/etc/ufw/before.rules" ]; then
+        sudo ufw delete $port/tcp
+    fi
 }
 
 function install_go(){
-	wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz -P /tmp
-	tar -C /usr/local -zxf /tmp/go1.14.1.linux-amd64.tar.gz
-	export GOPATH="/usr/go"
+    wget https://dl.google.com/go/go1.14.1.linux-amd64.tar.gz -P /tmp
+    tar -C /usr/local -zxf /tmp/go1.14.1.linux-amd64.tar.gz
+    export GOPATH="/usr/go"
+}
+
+function input_port(){
+    while true
+        do
+        read -p "请输入监听端口[1-65535]（默认8989）:" port
+        [[ -z "${port}" ]] && port="8989"
+        echo $((${port}+0)) &>/dev/null
+        if [[ $? -eq 0 ]]; then
+            if [[ ${port} -ge 1 ]] && [[ ${port} -le 65535 ]]; then
+                echo "设置端口:${port}"
+                break
+            else
+                echo "输入错误, 请输入正确的端口."
+            fi
+        else
+            echo "输入错误, 请输入正确的端口."
+        fi
+        done
+}
+
+function change_port(){
+    stop
+    sleep 2
+    input_port
+    del_post
+    chk_firewall
+    cd $dir && sed -i "4s/[0-9]\{1,5\}/$port/g" settings.toml
+    start
 }
 
 function get_speedtest(){
-	if [ -e $dir"speedtest" ]; then
-		echo "已经安装，将更新到最新版。"
-		rm -rf $dir
-	fi
-	install_go
-	cd && git clone https://github.com/librespeed/speedtest
-	cd speedtest
-	git checkout remotes/origin/go
+    if [ -e $dir"speedtest" ]; then
+        echo "已经安装，将更新到最新版."
+        rm -rf $dir
+    fi
+    install_go
+    cd && git clone https://github.com/librespeed/speedtest
+    cd speedtest
+    git checkout remotes/origin/go
     mkdir $dir && cp -r settings.toml assets $dir
-	/usr/local/go/bin/go build -o speedtest main.go
-	cp ./speedtest $dir
-	cd && rm -rf speedtest go
-	cd $dir"assets"
-	mv example-singleServer-full.html index.html
-	rm -rf /usr/local/go /usr/go
+    /usr/local/go/bin/go build -o speedtest main.go
+    cp ./speedtest $dir
+    cd && rm -rf speedtest go
+    cd $dir && sed -i "4s/[0-9]\{1,5\}/$port/g" settings.toml
+    cd $dir"assets" && mv example-singleServer-full.html index.html
+    rm -rf /usr/local/go /usr/go
 }
 
 function start(){
-	PID=`pgrep speedtest`
-	if [ ! -z $PID ]; then
-		echo "已经启动."
-		return
-	else
-		cd $dir && nohup ./speedtest > /var/log/speedtest.log 2>&1 &
-		echo "------------------------------------------------"
-		echo "启动成功."
-		echo "访问IP:8989测速."
-	fi
-	
+    PID=`pgrep speedtest`
+    if [ ! -z $PID ]; then
+        echo "已经启动."
+        return
+    else
+        cd $dir && nohup ./speedtest > /var/log/speedtest.log 2>&1 &
+        echo "------------------------------------------------"
+        echo "启动成功."
+        echo "访问IP:$port测速."
+    fi
+    
 }
 
 function stop(){
-	PID=`pgrep speedtest`
-	if [ ! -z ${PID} ]; then
-		kill -9 ${PID}
-		echo "停止成功."
-	else
-		echo "没有启动."
-	fi
-	
+    PID=`pgrep speedtest`
+    if [ ! -z ${PID} ]; then
+        kill -9 ${PID}
+        echo "停止成功."
+    else
+        echo "没有启动."
+    fi
 }
 
 
 function del(){
-	kill -9 $(pgrep 'speedtest')
-	del_post
-	rm -rf $dir
-	rm -f /var/log/speedtest.log
-	echo "卸载成功."
+    stop
+    del_post
+    rm -rf $dir
+    rm -f /var/log/speedtest.log
+    echo "卸载成功."
 }
 
 echo "------------------------------------------------"
 echo "Speedtest go版本一键安装管理脚本"
-echo "1) 安装 Speedtest"
-echo "2) 卸载 Speedtest"
-echo "3) 启动 Speedtest"
-echo "4) 停止 Speedtest"
+echo "1、安装 Speedtest"
+echo "2、卸载 Speedtest"
+echo "3、修改监听端口"
+echo "4、启动 Speedtest"
+echo "5、停止 Speedtest"
 echo "其它键退出！"
 read -p ":" istype
 case $istype in
-    1) 
+    1)
+    input_port
     setout
     get_speedtest
     chk_firewall
@@ -118,8 +148,10 @@ case $istype in
     2)
     del;;
     3)
+    change_port;;
+    4)
     start;;
-    4) 
-   	stop;;
+    5)
+    stop;;
     *) break
 esac
